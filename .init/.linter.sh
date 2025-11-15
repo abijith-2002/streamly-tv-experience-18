422ea4f760afabcb82b4d7574e3a7483ce2fe583
Kavia AI <production@kavia.ai>
2025-11-15 08:05:41 +0000

    Checkpoint 2 - cgc44dd35c

--- .init/.linter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/kavia/workspace/code-generation/streamly-tv-experience-18/android_tv_frontend
-./gradlew lint
-LINT_EXIT_CODE=$?
-if [ $LINT_EXIT_CODE -ne 0 ]; then
-   exit 1
-fi
-
